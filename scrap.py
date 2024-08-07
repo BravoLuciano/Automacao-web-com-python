4917bdac1131d79297905ee28f0ffa61f8d4a547
Luciano <bravoluciano@hotmail.com>
2024-08-06 21:21:37 -0300

    buscando o titulo do Livro e voltando para tela inicial

---
+++ scrap.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get('https://books.toscrape.com/')
+
+titulo = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/section/div[2]/ol/li[1]/article/h3/a')
+titulo.click()
+sleep(1)
+
+titulo_especifico = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div[2]/article/div[1]/div[2]/h1')
+print(titulo_especifico.text)
+
+driver.back()
+sleep(5)
