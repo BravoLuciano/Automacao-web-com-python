4a04a326bba4aeca59e0b09d4f21ceb78c1a4d21
Luciano <bravoluciano@hotmail.com>
2024-08-06 21:44:41 -0300

    buscando mais titulos

--- scrap.py
+++ scrap.py
@@ -16,3 +16,12 @@ print(titulo_especifico.text)
 
 driver.back()
 sleep(5)
+
+titulo2 = driver.find_element(By.XPATH, '/html/body/div/div/div/div/section/div[2]/ol/li[14]/article/h3/a')
+titulo2.click()
+sleep(1)
+
+titulo3 = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div[2]/article/div[1]/div[2]/h1')
+print(titulo3.text)
+driver.back()
+sleep(5)
